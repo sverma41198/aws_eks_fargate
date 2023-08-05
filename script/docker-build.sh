20a57ce8130265976b9f0d1c663ad0238e26a9ee
Sourabh Verma <sverma@msystechnologies.com>
2023-08-05 08:25:40 +0000

    Updated

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ..
-cp docker/Dockerfile app/
+cp docker/Dockerfile source/
 echo "Building Docker Image"
 echo -en '\n'
-docker build --no-cache -t  $2  ./app/
+docker build --no-cache -t  $2  ./source/
 echo -en '\n'
 docker tag app-docker-local:latest 028677550726.dkr.ecr.ap-south-1.amazonaws.com/$2:$1
 echo -en '\n'
