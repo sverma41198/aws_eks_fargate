49b08944aa18f74feef5ca7cbef1c5fad12ff866
sverma41198 <120069791+sverma41198@users.noreply.github.com>
2023-08-07 15:15:18 +0530

    Update docker-build.sh

--- script/docker-build.sh
+++ script/docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ..
-cp docker/Dockerfile source/
+cp docker/Dockerfile app/
 echo "Building Docker Image"
 echo -en '\n'
-docker build --no-cache -t  $2  ./source/
+docker build --no-cache -t  $2  ./app/
 echo -en '\n'
 docker tag app-docker-local:latest 028677550726.dkr.ecr.ap-south-1.amazonaws.com/$2:$1
 echo -en '\n'
